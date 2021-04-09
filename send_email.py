91c93d2cdec1f4b546aa766d7e7000b020541f2e
Sakib75 <skb09skb@gmail.com>
2021-04-09 06:25:49 +0600

    new commit

--- send_email.py
+++ send_email.py
@@ -11,10 +11,15 @@ password = "sakib7575"
    
 
 def Check_Dif(city):
-	df2 = pd.read_csv(f'{city}_old.csv')
-	df1 = pd.read_csv(f'{city}.csv')
+	try:
+		df2 = pd.read_csv(f'{city}_old.csv')
+		df1 = pd.read_csv(f'{city}.csv')
+	except:
+		return None
 
 	dif_list = [x for x in list(df1['heading'].unique()) if x not in list(df2['heading'].unique())]
+	print(dif_list)
+	print(len(dif_list))
 
 	dfdif = df1[(df1['heading'].isin(dif_list))]
 
@@ -26,45 +31,35 @@ def Check_Dif(city):
 	return {'Diff':diff,'df' : dfdif}
 
 
-new = Check_Dif('Essen')
 
-df_test = new['df'].reset_index()[['heading','website','status']].fillna('')
-
-html = """\
-<html>
-  <head>
-  
-  </head>
-  <body>
-
-  <center><h1> {0} New item found! </h1> </br>
-    {1}
-    </center>
-  </body>
-</html>
-""".format(len(df_test),df_test.to_html(classes='df'))
 
 def SendMail():
+	new = Check_Dif('Essen')
+	if(new):
 
-	msg = EmailMessage()
-	msg['Subject'] = 'Update for today!'
-	msg['From'] = sender
-	msg['To'] = receiver
-	msg.set_content('There are two new listing found!')
-	msg.add_alternative(html,subtype='html')
+		df_test = new['df'].reset_index()[['heading','website','status']].fillna('')
 
-	with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+		html = """\
+		<html>
+		  <head>
+		  
+		  </head>
+		  <body>
 
+		  <center><h1> {0} New item found! </h1> </br>
+		    {1}
+		    </center>
+		  </body>
+		</html>
+		""".format(len(df_test),df_test.to_html(classes='df'))
 
+		msg = EmailMessage()
+		msg['Subject'] = 'Update for today!'
+		msg['From'] = sender
+		msg['To'] = receiver
+		msg.set_content('There are two new listing found!')
+		msg.add_alternative(html,subtype='html')
 
-		smtp.login(sender,password)
-
-		subject = 'Grab dinner?'
-		body = 'How about?'
-
-
-
-		smtp.send_message(msg)
-		# smtp.sendmail(sender,sender,msg)
-
-SendMail()
+		with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
+			smtp.login(sender,password)
+			smtp.send_message(msg)
